e6e7e0a148c26ddc70e1643263c2853454ec815f
woniuxia <yumenawei@qq.com>
2026-08-26 11:09:18 +0800

    chore: 更新关注事项组件类型

--- apps/desktop/components.d.ts
+++ apps/desktop/components.d.ts
@@ -91,6 +91,7 @@ declare module 'vue' {
     ExceptionStackPanel: typeof import('./src/components/ExceptionStackPanel.vue')['default']
     FileLockPanel: typeof import('./src/components/FileLockPanel.vue')['default']
     FollowUpPanel: typeof import('./src/components/follow-up/FollowUpPanel.vue')['default']
+    FollowUpQuickInputs: typeof import('./src/components/follow-up/FollowUpQuickInputs.vue')['default']
     FormatterPanel: typeof import('./src/components/FormatterPanel.vue')['default']
     GlobalNotificationPopup: typeof import('./src/components/GlobalNotificationPopup.vue')['default']
     HomePanel: typeof import('./src/components/HomePanel.vue')['default']
