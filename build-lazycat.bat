2837c52416f9104ce0696b556d93fc1296ad3544
woniuxia <yumenawei@qq.com>
2026-08-28 21:02:06 +0800

    chore: 清理历史遗留文件

--- build-lazycat.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=x64
-set PATH=C:\Strawberry\perl\bin;%PATH%
-cd /d E:\Projects\LazyCat
-pnpm --filter @lazycat/desktop build:tauri
